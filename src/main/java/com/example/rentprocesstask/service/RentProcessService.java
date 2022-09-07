6a038d2fd6c331c7b2b7febb015f78d038b89bd3
Kaweesha23 <kaweeshanadeethara97@gmail.com>
2022-09-07 16:36:02 +0530

    Initial commit

---
+++ src/main/java/com/example/rentprocesstask/service/RentProcessService.java
@@ -0,0 +1,5 @@
+package com.example.rentprocesstask.service;
+
+public interface RentProcessService {
+    boolean validateDL(String dlNumber);
+}
---
+++ src/test/java/com/example/rentprocesstask/RentProcessTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentprocesstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentProcessTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
